20caa7af523bfcdd927675276ea643e11394d7f5
Dina <dina.fxx@gmail.com>
2025-10-24 21:16:48 +0600

    Friday lesson

---
+++ thirdLesson.py
@@ -0,0 +1,47 @@
+# products = ['water','chocolate','nuts' ]
+# person1=input('What do you want to buy?')
+# person2=(f"I want to {products[2]}")
+# print=(f'{products[2]}')
+# person1=input('How much?')
+# person2=(f'I need [1]')
+# person1=input('it will be cost 700')
+price = 700
+print(price)
+
+
+
+
+
+
+
+price = int(200)
+print('Change is',500-price)
+
+
+# price=int(input('enter price: ' ))
+# print('Change is'. 500-(price))
+
+
+
+
+
+
+
+
+a=5
+b=2
+c=10
+print(a*b/c)
+
+a=2
+b=3
+c=1
+print(a*(b-c))
+
+print((5-1)*(5+1))
+print(0.3*(4-1))
+print((91-1)/(2+1))
+print(((5*4)/3)**0.5)
+print (((1/3)+5)*((0.2-0.001)/91))
+print(((40-(4.25*7.08+6.768/0.75))*2050))
+print((0.16*(3.2-(3/40)))+(2*(3/11)*4.125/(3*(3/4)/((5*(1/6)*0.3-0.3*4.5+(1/3)*0.3))))
